ba696ca7ab371272c44bcaf3cdde2460937a56ef
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-06-29 00:14:15 +0600

    first commit

---
+++ helpers/data.js
@@ -0,0 +1,26 @@
+module.exports=[
+    {
+        name:"Product 1",
+        description:"Product 1 Description",
+        quantity:2,
+        price:20,
+    },
+    {
+        name:"Product 2",
+        description:"Product 2 Description",
+        quantity:3,
+        price:323,
+    },
+    {
+        name:"Product 3",
+        description:"Product 3 Description",
+        quantity:4,
+        price:2222,
+    },
+    {
+        name:"Product 4",
+        description:"Product 4 Description",
+        quantity:4,
+        price:120,
+    },
+]
